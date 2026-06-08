399ee299bc0e6414b085bd8065f322635854cc6d
Andrzej <111933899+Endixendi@users.noreply.github.com>
2026-06-08 02:22:26 +0200

    Refactor script.js to improve readability and structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -135,7 +135,7 @@ let currentSeasonalIcon = "❄️"; // Domyślna ikona awaryjna
     const donateBtn = document.getElementById("donate-btn");
     if (donateBtn) {
       donateBtn.addEventListener("click", () => {
-        window.open("https://streamelements.com/endixendi/tip", "_blank");
+        window.open("tipply.pl/@endixendi", "_blank");
       });
     }
   }
